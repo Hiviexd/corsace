c221a496e8d28f51eabda19fb225ee637f8f25d4
VINXIS <oykxf2@gmail.com>
2023-07-27 10:32:25 -0600

    create cache functions

--- Server/cache.ts
+++ Server/cache.ts
@@ -1,7 +1,23 @@
 import { LRUCache } from "lru-cache";
 
-const cache = new LRUCache<string, any>({
+export const cache = new LRUCache<string, any>({
     max: 100,
 });
 
-export { cache };
+export function deleteMCACache (data: { year: number }) {
+    return deleteEndpoints([
+        "/api/mcaInfo/front?year=" + data.year,
+        "/api/mca?year=" + data.year,
+        "/api/staff",
+    ]);
+}
+
+export function deleteEndpoints (endpoints: string[]) {
+    const result: string[] = [];
+    for (const endpoint of endpoints) {
+        const res = cache.delete(endpoint);
+        if (!res)
+            result.push(endpoint);
+    }
+    return result;
+}
